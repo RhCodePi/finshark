290aed26920dc9fb862d7fce120c2b125fceebdd
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-07-15 19:12:00 +0300

    was added register controller for user sign-up

---
+++ api/Migrations/20240715160751_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "32564d9b-d0f1-4d5c-94fa-1a4858a5d8f7", null, "Admin", "ADMIN" },
+                    { "6b7418b7-8686-48f9-8481-bfc40d90fc24", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "32564d9b-d0f1-4d5c-94fa-1a4858a5d8f7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6b7418b7-8686-48f9-8481-bfc40d90fc24");
+        }
+    }
+}
